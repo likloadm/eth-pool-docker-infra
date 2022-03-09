67b576b778e8eca1d9159c7013e3413e4d2ebedb
Vasilii <Bobit300@gmail.com>
2022-03-09 19:32:03 +0300

    Update run-geth.sh

--- geth/run-geth.sh
+++ geth/run-geth.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 echo "Starting geth-01..."
-screen -dmS geth-01 /usr/bin/geth --datadir ./data --port 42370 --rpc --rpcport 13270 --cache=1024
+screen -dmS geth-01 /usr/bin/geth --datadir ./data --port 42370 --http --http.port 13270 --cache=1024
 echo "Done."
 
 exit 0
